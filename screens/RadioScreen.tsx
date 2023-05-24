d051acecd3fb2ae6cc681bbd77a399ebc90a6165
jtimko <justin@jtimko.com>
2023-05-23 19:07:35 -0700

    Updating Track and Artist

--- screens/RadioScreen.tsx
+++ screens/RadioScreen.tsx
@@ -1,49 +1,55 @@
-import { useEffect, useState } from 'react';
-import TrackPlayer from 'react-native-track-player';
-import { Button, SafeAreaView, ScrollView, Text, View } from 'react-native';
+import { useEffect, useState } from "react";
+import TrackPlayer, {
+  Event,
+  useTrackPlayerEvents,
+} from "react-native-track-player";
+import { Button, SafeAreaView, ScrollView, Text, View } from "react-native";
 
 export default function RadioScreen() {
   const [isPlaying, setIsPlaying] = useState<boolean>(false);
+  const [trackTitle, setTrackTitle] = useState<string>("");
 
   useEffect(() => {
     async function startRadio() {
       await TrackPlayer.setupPlayer();
 
-      const data = await fetch('http://localhost:3000/radio');
+      const data = await fetch("http://localhost:3000/radio");
       const resp = await data.json();
       const url = resp.radio;
 
       var track1 = {
         url: url, // Load media from the network
-        title: 'Avaritia',
-        artist: 'HellaPunk Radio',
-        album: 'HellaPunk Radio',
-        // genre: 'Progressive House, Electro House',
-        // date: '2014-05-20T07:00:00+00:00', // RFC 3339
-        // artwork: 'http://example.com/cover.png', // Load artwork from the network
-        duration: 402 // Duration in seconds
+        title: "HellaPunk Radio",
+        artist: "HellaPunk Radio",
+        album: "HellaPunk Radio",
+        duration: 402, // Duration in seconds
       };
       TrackPlayer.add(track1);
+
+      TrackPlayer.addEventListener(Event.PlaybackMetadataReceived, async (event) => {
+          const title = event.artist + " - " + event.title;
+          if (title) setTrackTitle(title);
+        }
+      );
     }
     startRadio();
   }, []);
 
   function radioControl() {
     setIsPlaying(!isPlaying);
-    if (isPlaying) 
-      TrackPlayer.play();
-    else 
-      TrackPlayer.pause();
+    if (isPlaying) TrackPlayer.play();
+    else TrackPlayer.pause();
   }
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-        >
-          <Text>Radio</Text>
-          <Button onPress={() => radioControl()} title={isPlaying ? "Pause" : "Play"} />
+        <ScrollView contentInsetAdjustmentBehavior="automatic">
+          <Button
+            onPress={() => radioControl()}
+            title={isPlaying ? "Pause" : "Play"}
+          />
+          {trackTitle != "" ? <Text>Now Playing: {trackTitle}</Text> : null}
         </ScrollView>
       </SafeAreaView>
     </View>
